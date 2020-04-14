aad328962050acf7232f764072c5b1c0da08a6ec
jiuras <57357540+jiuras@users.noreply.github.com>
2020-04-14 14:12:03 +0200

    Update opgave.js

--- opgave.js
+++ opgave.js
@@ -1,2 +1,12 @@
-var array1 = [1, 2, 3];
-var array2 = [4, 5, 6];
+let array1 = [1, 2, 3];
+let array2 = [4, 5, 6];
+let combined = [];
+
+for (i = 0; i < array1.length; i++) {
+    for (j = 0; j < array2.length; j++) {
+        if(array1[i] + array2[j] == 8) {
+        combined.push([array1[i], array2[j]]);
+        }
+    }
+}
+console.log(combined);
